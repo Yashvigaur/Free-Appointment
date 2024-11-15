694c1634507d3607036556cf2bca33e375d57ad5
Yashvi Gaur <yashvigaur2111@gmail.com>
2024-11-15 20:12:00 +0530

    updated

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -60,20 +60,20 @@ function profile() {
           <option value="Male">Male</option>
           <option value="Female">Female</option>
           </select>
-          : <p>{userData.gender}</p>
+          : <p className='text-gray-400'>{userData.gender}</p>
         }
-        <p>Birthday:</p>
+        <p className='font-medium '>Birthday:</p>
         {
-          isEdit? <input type="date" onChange={(e)=>setUserData(prev=>({...prev,dob:e.target.value}))}  value={userData.dob}/>
-          : <p>{userData.dob}</p>
+          isEdit? <input className='max-w-28 bg-gray-100' type="date" onChange={(e)=>setUserData(prev=>({...prev,dob:e.target.value}))}  value={userData.dob}/>
+          : <p className='text-gray-400'>{userData.dob}</p>
         }
       </div>
      </div>
 
-     <div>
+     <div className='mt-10'>
       {
-        isEdit?<button onClick={()=>setIsEdit(false)}>Save information</button>
-        : <button onClick={()=>setIsEdit(true)}>Edit</button>
+        isEdit?<button className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all' onClick={()=>setIsEdit(false)}>Save information</button>
+        : <button className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all' onClick={()=>setIsEdit(true)}>Edit</button>
       }
      </div>
 
